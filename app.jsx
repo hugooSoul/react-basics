b7428936b70dadb6e9d9c854f0d879511efdf149
Hugo <hugol.soul@gmail.com>
2018-01-07 21:05:25 +0000

    Understainding DOM & components

---
+++ app.jsx
@@ -0,0 +1,10 @@
+function Application () {
+  return (
+    <div>
+      <h1>Hi @hugooSoul</h1>
+      <p>Render from Application component</p>
+    </div>
+  );
+}
+
+ReactDOM.render(<Application />, document.getElementById('container'));
